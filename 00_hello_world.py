446325d3f902226434e9cf9b9216c280abdc51d6
Yerman Avila <65168940+yavilag-SENA@users.noreply.github.com>
2023-10-17 09:13:29 -0500

    Add files via upload

---
+++ 00_hello_world.py
@@ -0,0 +1,20 @@
+# Primer programa que escribes en un nuevo lenguaje
+print("Hello, World!")
+
+# Esto es un comentario, es texto que ayuda a documentar y entender el código. No se imprime ni se ejecuta
+# Puede escribirse en líneas con # inicial o múltiples líneas como aparece a continuación
+
+"""
+Por lo general el Hello World sólo muestra las palabras Hello, World!, en cada lenguaje tiene diferentes 
+formas de escribirlo que es lo que llamaremos Sintaxis. 
+En este lenguaje únicamente se usa la función de mostrar en pantalla que es: 
+
+print()
+
+Y esta función recibe como parámetro o argumento el texto o valor que deseo visualizar en la pantalla o consola. 
+
+El programa se vería únicamente así: 
+print("Hello, World!")
+"""
+
+
